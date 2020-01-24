7952fd877b633aeba7477b5d958ba9c1d089509b
Lucas004 <55288398+Lucas004@users.noreply.github.com>
2020-01-24 11:33:06 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -1,10 +1,20 @@
 
-function myloop(){
-var end = document.getElementById("end").value;
-var start = document.getElementById("start").value;
+function myFunction() {
 
-for(var i = start; i <= end; i++)
-  {
-    document.write("<p>" + i);
-  }
-  }
+var start = document.getElementById("start").value;
+var end = document.getElementById("end").value;
+var jump = document.getElementById("jump").value;
+
+console.log(typeof start);  
+console.log(typeof end);
+console.log(typeof jump);
+  
+ start= Number(start);
+ end= Number(end);
+ jump=Number(jump);
+  
+  for (var i = start; i <= end; i+=jump) {
+    
+ document.write( '<p>' + i + '</p>');
+}
+}
